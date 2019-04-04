144fedf62ab0f4d8ad3b2553a677dbb735714a1b
Pham Van Doanh <doanhpv2@evolableasia.vn>
2019-04-04 09:54:16 +0700

    laravel

--- setup_docker.sh
+++ setup_docker.sh
@@ -1,17 +1,17 @@
 detach_mode=$1
 
 echo "Install docker"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
-sudo apt-get update;
-sudo apt-get install -y docker-ce;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+apt-get update;
+apt-get install -y docker-ce;
 
 echo "Add user to group docker"
-sudo usermod -aG docker ${USER};
+usermod -aG docker ${USER};
 
 echo  "Install docker-compose"
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
-sudo chmod +x /usr/local/bin/docker-compose;
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose;
+chmod +x /usr/local/bin/docker-compose;
 
 echo "Run docker"
 cp docker/.env.example docker/.env;
