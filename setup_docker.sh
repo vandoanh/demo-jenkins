f8951c297ae57df3b0d64ee348ed07ad407852c9
Pham Van Doanh <doanhpv2@evolableasia.vn>
2019-04-04 16:57:06 +0700

    fdfdfd

--- setup_docker.sh
+++ setup_docker.sh
@@ -12,42 +12,43 @@ fi
 ###########################################################################
 # Setup project
 ###########################################################################
-
-docker-compose exec -T php-fpm cp .env.docker .env;
+docker-compose exec php-fpm bash
+cp .env.docker .env;
 
 echo "Run composer install.";
-docker-compose exec -T php-fpm composer install;
+composer install;
 
 echo "Create App key.";
-docker-compose exec -T php-fpm php artisan key:generate --ansi;
+php artisan key:generate --ansi;
 
 echo "Create storage folder and sub folder: app, framework (cache, sessions, views), logs.";
-docker-compose exec -T php-fpm mkdir storage;
-docker-compose exec -T php-fpm mkdir storage/app;
-docker-compose exec -T php-fpm mkdir storage/framework;
-docker-compose exec -T php-fpm mkdir storage/framework/cache;
-docker-compose exec -T php-fpm mkdir storage/framework/sessions;
-docker-compose exec -T php-fpm mkdir storage/framework/views;
-docker-compose exec -T php-fpm mkdir storage/logs;
+mkdir storage;
+mkdir storage/app;
+mkdir storage/framework;
+mkdir storage/framework/cache;
+mkdir storage/framework/sessions;
+mkdir storage/framework/views;
+mkdir storage/logs;
 
 echo "Change mod write 777 for folder and sub of bootstrap and storage folder.";
-docker-compose exec -T php-fpm chmod -R 777 bootstrap/cache storage;
+chmod -R 777 bootstrap/cache storage;
 
 echo "Run npm install -g pm2.";
-docker-compose exec -T php-fpm npm install -g pm2
+npm install -g pm2
 
 echo "Run npm install && npm run production.";
-docker-compose exec -T php-fpm npm install;
-docker-compose exec -T php-fpm npm run production;
+npm install;
+npm run production;
 
 echo "Run pm2 start queue.sh to monitor queue worker.";
-docker-compose exec -T php-fpm pm2 start queue.sh -l --log storage/logs/worker.log
+pm2 start queue.sh -l --log storage/logs/worker.log
 
 echo "Run migrate & db seed"
-docker-compose exec -T php-fpm php artisan migrate;
-docker-compose exec -T php-fpm php artisan passport:install;
-docker-compose exec -T php-fpm php artisan db:seed;
-docker-compose exec -T php-fpm php artisan es:index-mapping;
-docker-compose exec -T php-fpm php artisan crawler:news;
-docker-compose exec -T php-fpm pm2 monit;
+php artisan migrate;
+php artisan passport:install;
+php artisan db:seed;
+php artisan es:index-mapping;
+php artisan crawler:news;
+exit;
+# pm2 monit;
 
